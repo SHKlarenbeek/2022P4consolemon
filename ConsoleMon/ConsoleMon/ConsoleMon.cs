3ac599da043b9bf738dddb666f1c156c951951df
SHKlarenbeek <34334@ma-web.nl>
2022-05-09 10:33:26 +0200

    ConsoleMon

---
+++ ConsoleMon/ConsoleMon/ConsoleMon.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMon
+{
+    internal class ConsoleMon
+    {
+        private string EnemyName;
+        private int EnemyHP;
+        private int EnemyEnergy;
+
+        internal void TakeDamge(int damage)
+        {
+            EnemyHP = EnemyHP - damage;
+            //for (int damage = 0; damage < 10; damage++)
+            //{
+            //   int EnemyHP = 0;
+            //    EnemyHP += damage;
+            //    Console.WriteLine(EnemyHP);
+            //
+            //    EnemyHP = EnemyHP - damage;
+            //}
+        }
+        internal void DepleteEnergy(int energy)
+        {
+            EnemyEnergy = EnemyEnergy - energy;
+        }
+    }
+}
---
+++ ConsoleMon/ConsoleMon/Skill.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMon
+{
+    internal class Skill
+    {
+        private string MoveName;
+        private int MoveDMG;
+        private int MoveEnergy;
+
+        internal void UseOn(ConsoleMon caster, ConsoleMon target)
+        {
+            caster.DepleteEnergy(MoveEnergy);
+            target.TakeDamge(MoveDMG);
+        }
+    }
+}
